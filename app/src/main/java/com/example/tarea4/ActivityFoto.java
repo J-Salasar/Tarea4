49705155893f1b8d91db678081fc03e21a246909
Salasar001 <salasar1820@gmail.com>
2022-10-27 16:53:04 -0600

    Ejercicio completo

--- app/src/main/java/com/example/tarea4/ActivityFoto.java
+++ app/src/main/java/com/example/tarea4/ActivityFoto.java
@@ -1,14 +1,23 @@
 package com.example.tarea4;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.ImageView;
+import java.io.File;
 public class ActivityFoto extends AppCompatActivity {
-
+    private ImageView foto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foto);
+        foto=(ImageView) findViewById(R.id.img_foto);
+        File pegar=new File(getIntent().getStringExtra("foto"));
+        foto.setImageURI(Uri.fromFile(pegar));
+    }
+    public void retroceder(View view){
+        Intent inicio=new Intent(this,ActivityLista.class);
+        startActivity(inicio);
     }
 }
